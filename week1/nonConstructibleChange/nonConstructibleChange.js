4f684296e8f537ca0a7d1993e646af03cc6ea0f9
Aleksandar Tanev <91281906+AleksTanev@users.noreply.github.com>
2022-08-22 22:14:35 +0300

    Added solution for nonConstructibleChange

--- week1/nonConstructibleChange/nonConstructibleChange.js
+++ week1/nonConstructibleChange/nonConstructibleChange.js
@@ -1,60 +1,45 @@
-function findMinimumNonConstructibleChange(arrayOfCoins) {
+async function findMinimumNonConstructibleChange(arrayOfCoins) {
 
-  // const buildBSTFromArr = (arrayOfCoins, start = 0, end = arrayOfCoins.length - 1) => {
+  const sortedArr = arrayOfCoins.sort((a,b) => a-b);
 
-  //   if (start === end) {
-  //     return;
-  //   }
-
-
-
-  // }
-
-  if (arrayOfCoins.length < 1) {
-    return [];
+  let i = 0;
+  if (sortedArr[i] !== 1) {
+    return 1;
   }
 
-
-
-  let index = 0;
-  let root = newNode(arrayOfCoins[index++])
-
-  while (index < arrayOfCoins.length) {
-    const node = newNode(arrayOfCoins[index]);
-
-    buildBST(root, node);
-    index++;
+  let sum = 0;
+  while (i < sortedArr.length) {
+    if (sortedArr[i] > sum + 1) {
+      return sum + 1;
+    }
+    sum += sortedArr[i]
+    i++;
   }
 
-  return root;
-}
-
-const newNode = (value) => {
-
-  return {
-    value,
-    left: null,
-    right: null
-  }
-}
-
-const buildBST = (root, newNode) => {
-
-  if (!root.left && root.value && root.value > newNode.value) {
-    root.left = newNode;
-    return;
-  }
-
-  if (!root.right && root.value && root.value <= newNode.value) {
-    root.right = newNode;
-    return;
-  }
-
-  if (root.value > newNode.value) buildBST(root.left, newNode)
-  if (root.value <= newNode.value) buildBST(root.right, newNode)
-
+  return sum + 1;
 }
 
 //// TESTS
-const coins = [5, 7, 1, 1, 2, 3, 22];
-console.log(findMinimumNonConstructibleChange(coins));
+{
+  const coins = [5, 7, 1, 1, 2, 3, 22];
+  const expected = 20
+  console.log(findMinimumNonConstructibleChange(coins));
+};
+
+{
+  const coins = [1, 1, 4, 22];
+  const expected = 3
+  console.log(findMinimumNonConstructibleChange(coins));
+};
+
+{
+  const coins = [1, 4];
+  const expected = 2
+  console.log(findMinimumNonConstructibleChange(coins));
+};
+
+{
+  const coins = [];
+  const expected = 1
+  console.log(findMinimumNonConstructibleChange(coins));
+};
