b4d47c9d002ba05ed0d8d53e672ac4bcc0882c42
AlexTanev <alexander.tanev@yahoo.com>
2022-09-16 02:09:51 +0300

    improved insertion sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,14 +1,15 @@
 const insertionSort = (array) => {
 
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] > array[i+1]) {
-      [array[i+1], array[i]] = [array[i], array[i+1]];
-    }
+  for (let i = 0; i < array.length; i++) {
 
     for (let j = i; j >= 1; j--) {
+
       if (array[j] < array[j-1]) {
+
         [array[j], array[j-1]] = [array[j-1], array[j]];
+
       } else {
+        
         break;
       }
     }
